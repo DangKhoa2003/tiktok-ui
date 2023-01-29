a3d1c595d6dd04c0a47d96590b3b03f394e02865
DangKhoa2003 <tranhadangkhoa2003@gmail.com>
2023-01-29 13:51:12 +0700

    Xây dựng UI phần Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -2,16 +2,18 @@ import classNames from "classnames/bind";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./Header.module.scss";
 import images from "~/assets/images";
-import { faCircleXmark, faSearch, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import {
+    faCircleXmark,
+    faSearch,
+    faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx("wrapper")}>
             <div className={cx("inner")}>
-                <div className={cx("logo")}>
-                    <img src={images.logo} alt="TikTok" />
-                </div>
+                <img src={images.logo} alt="TikTok" />
                 <div className={cx("search")}>
                     <input
                         placeholder="Search accounts and videos"
@@ -19,16 +21,17 @@ function Header() {
                         type="text"
                     />
                     <button className={cx("clear")}>
-                        <FontAwesomeIcon icon={ faCircleXmark } />
+                        <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
-                    <FontAwesomeIcon className={cx("loading")} icon={ faSpinner } />
+                    <FontAwesomeIcon
+                        className={cx("loading")}
+                        icon={faSpinner}
+                    />
                     <button className={cx("search-btn")}>
-                        <FontAwesomeIcon icon={ faSearch } />
+                        <FontAwesomeIcon icon={faSearch} />
                     </button>
                 </div>
-                <div className={cx("actions")}>
-
-                </div>
+                <div className={cx("actions")}></div>
             </div>
         </header>
     );
